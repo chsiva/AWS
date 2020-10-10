9fa45169a0eef449c36bfe13304eb3deae9c362d
chsiva <36084965+chsiva@users.noreply.github.com>
2020-10-10 18:59:38 -0400

    Update ec2_stop_start.py

--- ec2_stop_start.py
+++ ec2_stop_start.py
@@ -1,8 +1,8 @@
 import boto3
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name='us-east-1')
-    ec2.stop_instances(InstanceIds=['i-034d80e0dbd354089'])
-    print('stopped your instances: ' + str('i-034d80e0dbd354089'))
+    ec2.stop_instances(InstanceIds=['i-***'])
+    print('stopped your instances: ' + str('i-***'))
     return
     
     
@@ -10,6 +10,6 @@ def lambda_handler(event, context):
 import boto3
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2', region_name='us-east-1')
-    ec2.start_instances(InstanceIds=['i-034d80e0dbd354089'])
-    print('started your instances: ' + str('i-034d80e0dbd354089'))
+    ec2.start_instances(InstanceIds=['i-***'])
+    print('started your instances: ' + str('i-***'))
     return
